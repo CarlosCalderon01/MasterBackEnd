175689db643d5d73f1a7109efc412020ff3956f2
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-11-10 18:33:29 -0500

    models in order

--- globalmastertable/models.py
+++ globalmastertable/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+# // ----- // ----- // ----- // ----- // ----- //
+# Section 1 - Country, State, City, location
+# // ----- // ----- // ----- // ----- // ----- //
+
 
 class GmtCountry(models.Model):
     name = models.TextField()
@@ -13,6 +17,23 @@ class GmtCountry(models.Model):
         db_table = 'gmtcountry'
 
 
+class GmtState(models.Model):
+    gmtcountry = models.ForeignKey(GmtCountry, models.DO_NOTHING)
+    name = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'gmtstate'
+
+# // ----- // ----- // ----- // ----- // ----- //
+# Section 1 - Finish Section
+# // ----- // ----- // ----- // ----- // ----- //
+
+# // ----- // ----- // ----- // ----- // ----- //
+# Section 2 - Language, Language, Communication
+# // ----- // ----- // ----- // ----- // ----- //
+
+
 class GmtLanguage(models.Model):
     name = models.TextField()
     alfa2 = models.CharField(max_length=2, blank=True, null=True)
@@ -21,11 +42,6 @@ class GmtLanguage(models.Model):
         managed = False
         db_table = 'gmtlanguage'
 
-
-class GmtState(models.Model):
-    gmtcountry = models.ForeignKey(GmtCountry, models.DO_NOTHING)
-    name = models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = 'gmtstate'
+# // ----- // ----- // ----- // ----- // ----- //
+# Section 2 - Finish Section
+# // ----- // ----- // ----- // ----- // ----- //
